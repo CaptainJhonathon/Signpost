7cd2822a3b893b5490e209fec2c77e7fa9f537e4
En-TecH <Jars9756482@outlook.com>
2025-06-19 10:47:45 +0800

    success test flask for all pages

--- back-end/webapp.py
+++ back-end/webapp.py
@@ -1,27 +1,30 @@
 from flask import Flask, render_template \
-    , send_from_directory
+    , send_from_directory, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='assets')
 # app.config.from_object("config")
 
 @app.route("/")
 @app.route("/home")
 def Home():
-    return render_template('index.html')
+    return render_template("home/index.html")
 
 @app.route("/document")
 def Document():
-    return render_template("debug.html", site_name="Document")
+    return render_template("document/index.html")
 
 @app.route("/replaypanel")
 def ReplayPanel():
-    return render_template("debug.html", site_name="ReplayPanel")
+    return render_template("replaypanel/index.html")
 
 @app.route("/about")
 def About():
-    return render_template("debug.html", site_name="About")
+    return render_template("about/index.html")
 
+@app.route("/lgrg")
+def LoginRegister():
+    return render_template("loginregister/index.html")
 
 # for this singal debug
 if __name__ == "__main__":
